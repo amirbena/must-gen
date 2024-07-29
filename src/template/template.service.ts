aad27d1e0c207b581049b1e7871a163c026aac93
Amir Benassayag <amir12061968@gmail.com>
2024-07-29 10:22:40 +0300

    add new ControllerTemplate

--- src/template/template.service.ts
+++ src/template/template.service.ts
@@ -19,7 +19,59 @@ export class TemplateService {
 
     async renderNewTemplate() {
         const api = await swaggerParser.parse(path.resolve(process.cwd(), "src", "swagger.yaml"));
-        console.log(api);
+        console.log(api.paths);
+        console.log(api.paths["/user/{id}"].delete);
+        
+      /*   const paths = api.paths;
+
+        console.log(api.tags);
+        const controllers = {};
+
+        for (const [routePath, methods] of Object.entries(paths)) {
+            for (const [method, details] of Object.entries(methods)) {
+                //@ts-ignore
+                const operationId = details.operationId;
+                if (operationId) {
+                    const controllerName = `${operationId.charAt(0).toUpperCase() + operationId.slice(1)}Controller`;
+                    const route = {
+                        method: method.toUpperCase(),
+                        path: routePath,
+                        handlerName: operationId,
+                        pathParams: (details as any).parameters?.filter(param => param.in === 'path') || [],
+                        queryParams: (details as any).parameters?.filter(param => param.in === 'query') || [],
+                        bodyParam: (details as any).parameters?.find(param => param.in === 'body')?.name,
+                        bodyType: (details as any).parameters?.find(param => param.in === 'body')?.schema?.$ref.split('/').pop()
+                    };
+
+                    if (!controllers[controllerName]) {
+                        controllers[controllerName] = {
+                            controllerName,
+                            routes: []
+                        };
+                    }
+                    controllers[controllerName].routes.push(route);
+                }
+            }
+        }
+
+        console.log(controllers); */
+
+       /*  const controllerTemplate = fs.readFileSync(path.join(__dirname, 'controller.mustache'), 'utf8');
+        for (const [controllerName, controllerData] of Object.entries(controllers)) {
+          const renderedController = mustache.render(controllerTemplate, {
+            //@ts-ignore
+            ...controllerData,
+            importedMethods: ['UseGuards', 'UseInterceptors'],
+            isBodyUsed: controllerData.routes.some(route => route.bodyParam),
+            isParamsUsed: controllerData.routes.some(route => route.pathParams.length > 0),
+            isQueriesUsed: controllerData.routes.some(route => route.queryParams.length > 0),
+            guards: [], // Add guards if necessary
+            interceptors: [] // Add interceptors if necessary
+          });
+          fs.writeFileSync(path.join(__dirname, `${controllerName}.ts`), renderedController);
+        } */
+
+        
     }
 
     generateFile(templatePath: string, data: any, outputPath: string) {
